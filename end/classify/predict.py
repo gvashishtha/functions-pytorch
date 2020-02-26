7773cdc7f77f372c636bf283cc3ff19a43c77798
Gopal K. Vashishtha <gopalv@microsoft.com>
2020-02-26 15:58:28 -0800

    form working

--- end/classify/predict.py
+++ end/classify/predict.py
@@ -1,8 +1,11 @@
-from urllib.request import urlopen
+from datetime import datetime
 from PIL import Image
 from torchvision import transforms
-import sys
+from urllib.request import urlopen
 
+import logging
+import os
+import sys
 import torch
 
 model = torch.hub.load('pytorch/vision:v0.5.0', 'resnet18', pretrained=True)
@@ -11,18 +14,24 @@ model.eval()
 def get_class_labels():
     class_dict = {}
     counter = 0
-    with open('labels.txt', 'r') as infile:
-        for line in infile.readlines():
-            out = line.split("'")
-            class_dict[counter] = out[1]
-            counter += 1
+    try:
+        dirname = os.path.dirname(__file__)
+        with open(os.path.join(dirname, 'labels.txt'), 'r') as infile:
+            for line in infile.readlines():
+                out = line.split("'")
+                class_dict[counter] = out[1]
+                counter += 1
+    except FileNotFoundError:
+        logging.info(os.listdir(os.curdir))
+        logging.info(os.curdir)
+        raise
+
     return class_dict
 
 def predict_image_from_url(image_url):
     class_dict = get_class_labels()
     with urlopen(image_url) as testImage:
-        input_image = Image.open(testImage)
-        
+        input_image = Image.open(testImage).convert('RGB')
         preprocess = transforms.Compose([
             transforms.Resize(256),
             transforms.CenterCrop(224),
@@ -42,9 +51,17 @@ def predict_image_from_url(image_url):
         # Tensor of shape 1000, with confidence scores over Imagenet's 1000 classes
         print(output[0])
         # The output has unnormalized scores. To get probabilities, you can run a softmax on it.
-        out = (torch.nn.functional.softmax(output[0], dim=0))
-        out = class_dict[out.argmax().items()]
-        return out
+        softmax = (torch.nn.functional.softmax(output[0], dim=0))
+        out = class_dict[softmax.argmax().item()]
+
+        response = {
+            'created': datetime.utcnow().isoformat(),
+            'predictedTagName': out,
+            'prediction': softmax.max().item()
+        }
+
+        logging.info(f'returning {response}')
+        return response
 
 if __name__ == '__main__':
     predict_image_from_url(sys.argv[1])
